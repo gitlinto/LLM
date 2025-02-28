eb523596888cc0b78fb5c3ae71dbf7d8b83d0d69
gitlinto <lintolucas@gmail.com>
2025-02-27 22:44:54 -0600

    Create 03_groqtest.py

---
+++ 00.basics/03_groqtest.py
@@ -0,0 +1,30 @@
+# 1. Do a pip install of langchain: pip install langchain-groq
+# 2. In an .env file setup GROQ_API_KEY=gsk-tqwgtqwt
+import time
+from langchain_groq import ChatGroq
+from dotenv import load_dotenv
+load_dotenv()
+
+# Model names
+models = {
+    "mixtral-8x7b-32768": "Mixtral 8x7b 32768",
+    "llama-3.1-70b-versatile": "Llama3.1 70b 8192",
+    "gemma-7b-it": "Gemma 7b IT"
+}
+
+# Test each model
+for model_name, model_display_name in models.items():
+    llm = ChatGroq(model_name=model_name)
+
+    start_time = time.time()
+    response = llm.invoke(
+        "Talk about your most favorite EPL football club in 2 sentences")
+    end_time = time.time()
+
+    # Calculate the elapsed time
+    elapsed_time = end_time - start_time
+
+    # Output the results
+    print(f"Model: {model_display_name}")
+    print(f"Response: {response}")
+    print(f"API call latency: {elapsed_time:.3f} seconds\n")
